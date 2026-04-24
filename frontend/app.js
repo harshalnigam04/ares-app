d5bd51f23d21d0dfe668613da8345bc5301f8b89
harshalnigam04 <hnigam33@gmail.com>
2026-04-24 22:19:41 +0530

    Initial commit

---
+++ frontend/app.js
@@ -0,0 +1,34 @@
+//express app that serves html file
+
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+const URL = process.env.BACKEND_URL || 'http//localhost:8080/api';
+
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+app.get('/', async function (req, res) {
+    const options = {
+        method: 'GET'
+    };
+    fetch(URL, options)
+        .then(req => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.error('error:' + err));
+    try {
+        let response = await fetch(URL, options);
+        response = await response.json();
+        res.render('index', response)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: 'Internal Server Error.' });
+    }
+});
+
+app.listen(3000, function () {
+    console.log('Ares listening on port 3000!');
+});
+   
